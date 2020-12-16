b3c01f4becdc3745b34bce2ff6d4c52ea88163f7
Mahesh Khatiwada <maheshkhatiwada17@gmail.com>
2020-12-16 12:14:08 +0545

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA86nUCfO90wc5JGDhnJ7cMNf8oP4iR4Bc",
+  authDomain: "clone-6cb8c.firebaseapp.com",
+  projectId: "clone-6cb8c",
+  storageBucket: "clone-6cb8c.appspot.com",
+  messagingSenderId: "573823311857",
+  appId: "1:573823311857:web:262e04f560e71842452e50"
+};
